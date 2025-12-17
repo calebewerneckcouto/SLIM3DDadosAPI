18bd10dcdc1d604089721fbc0c597494f0282e2b
calebewerneckcouto <calebewerneck@hotmail.com>
2025-12-17 18:18:23 -0300

    novo api key

--- src/main/java/com/devsuperior/cwcdev/DadosBot.java
+++ src/main/java/com/devsuperior/cwcdev/DadosBot.java
@@ -1,6 +1,6 @@
 package com.devsuperior.cwcdev;
 
 public class DadosBot {
-	 public static final String BOT_TOKEN = "7731078890:AAHQMD2jyXg35nmSY7_6Cws-XdU1nv7UdCE";
+	 public static final String BOT_TOKEN = "7731078890:AAF57eKmTbqPTymZao7xGXucvcYIh5CcpqY";
 	    public static final String BOT_USER_NAME = "slim3dbot";
 }
