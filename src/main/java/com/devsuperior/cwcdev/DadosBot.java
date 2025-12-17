0a110c97baa79d54e49101cf4cbfcd04a2f7a415
calebewerneckcouto <calebewerneck@hotmail.com>
2025-12-17 18:29:55 -0300

    teste

--- src/main/java/com/devsuperior/cwcdev/DadosBot.java
+++ src/main/java/com/devsuperior/cwcdev/DadosBot.java
@@ -1,6 +1,6 @@
 package com.devsuperior.cwcdev;
 
 public class DadosBot {
-	 public static final String BOT_TOKEN = "7780724276:AAGw-FJ0ApOmVenLQ0lBqfsFJIDF4KqMbPI";
-	    public static final String BOT_USER_NAME = "calebewerneckbot";
+	 public static final String BOT_TOKEN = "7563413606:AAG7jo7LFIMcmjdvrxARJRjktB8Kxjahdls";
+	    public static final String BOT_USER_NAME = "Myrandonbot";
 }
