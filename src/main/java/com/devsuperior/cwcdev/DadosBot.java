efc1492c9383a32c1044d7c38f521e0d7c0d3061
calebewerneckcouto <calebewerneck@hotmail.com>
2025-12-17 18:41:33 -0300

    enviando slim3dboot

--- src/main/java/com/devsuperior/cwcdev/DadosBot.java
+++ src/main/java/com/devsuperior/cwcdev/DadosBot.java
@@ -1,6 +1,6 @@
 package com.devsuperior.cwcdev;
 
 public class DadosBot {
-	 public static final String BOT_TOKEN = "7563413606:AAG7jo7LFIMcmjdvrxARJRjktB8Kxjahdls";
-	    public static final String BOT_USER_NAME = "Myrandonbot";
+	 public static final String BOT_TOKEN = "8576506200:AAHkaC62bOdo31FNBSY5FYRVSLra4eu8CSo";
+	    public static final String BOT_USER_NAME = "slim3dboot";
 }
