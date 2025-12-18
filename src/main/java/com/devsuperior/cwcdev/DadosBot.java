aa8427bd2cea27cfc1fc4feb106608a8acf1c4d2
CALEBE WERNECK COUTO <59000781+calebewerneckcouto@users.noreply.github.com>
2025-12-18 13:50:21 -0300

    Change BOT_TOKEN in DadosBot.java
    Updated the BOT_TOKEN to a new value.

--- src/main/java/com/devsuperior/cwcdev/DadosBot.java
+++ src/main/java/com/devsuperior/cwcdev/DadosBot.java
@@ -1,6 +1,6 @@
 package com.devsuperior.cwcdev;
 
 public class DadosBot {
-	 public static final String BOT_TOKEN = "8597232377:AAHksRGUvDxymzqHR-Kjr0lseZEx_vaKUW8";
+	 public static final String BOT_TOKEN = "8343400452:AAF1j31JMLnx8sfzy5XSfjdV4SLAk0-tCnY";
 	    public static final String BOT_USER_NAME = "slim3dbot";
 }
